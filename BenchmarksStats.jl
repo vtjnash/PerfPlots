f8ceccb33962eb9ad240c319cb821cc66faf421f
Jameson Nash <vtjnash@gmail.com>
2017-03-02 14:34:31 -0500

    add summarizing code

--- BenchmarksStats.jl
+++ BenchmarksStats.jl
@@ -4,8 +4,8 @@ using JLD
 using StatsBase
 using BenchmarkTools
 using Glob
+using HTTP
 # using Libz
-# using HTTP
 # using Base.LibGit2
 
 # push!(LOAD_PATH, @__DIR__)
@@ -65,13 +65,51 @@ function get_daily_summary(date)
     return JSON.parse(readstring(file))
 end
 
+# take a Vector{Dict{String, Float64}} and turn it into Dict{String, Vector{Float64}}
+# but with nesting and nullable
+function transpose_daily_summary(data)
+    groups = Set{String}()
+    for day in data
+        isa(day, Dict) && union!(groups, keys(day))
+    end
+
+    if isempty(groups)
+        output = Float64[ (day === nothing ? NaN : day) for day in data ]
+    else
+        output = Dict{String, Any}()
+        sizehint!(output, length(groups))
+        for group in groups
+            values = Any[ (isa(day, Dict) ? get(day, group, nothing) : day) for day in data ]
+            output[group] = transpose_daily_summary(values)
+        end
+    end
+    return output
+end
+
+# take a Dict{String, Dict{String, ...{String, Vector{Float64}}}
+# and flatten it into Dict{String, Vector{Float64}} by name-mangling the paths together
+flatten_summary(data) = flatten_summary("", data)
+flatten_summary(prefix::String, data::Vector{Float64}) = Dict(prefix => data)
+function flatten_summary(prefix::String, data::Dict)
+    output = Dict{String, Vector{Float64}}()
+    for (k, v) in data
+        merge!(output, flatten_summary(isempty(prefix) ? k : "$prefix.$k", v))
+    end
+    return output
+end
+
 if false
     today = trunc(now(), Dates.Day)
-    until = today - Dates.Day(3)
-    summaries = Dict{String, Any}()
+    until = today - Dates.Day(90)
+    headers = String[]
+    data = Any[]
     while today > until
         date = datedirname(today)
-        summaries[date] = get_daily_summary(date)
+        push!(headers, date)
+        push!(data, get_daily_summary(date))
         today -= Dates.Day(1)
     end
+    missing = data .== nothing
+    data = transpose_daily_summary(data)
+    data = flatten_summary(data)
 end
